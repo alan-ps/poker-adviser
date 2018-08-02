995842b8b4708c18d2b40220a8e9dc6cd28746e5
Serhii Puchkovskii <serhiipuchkovskii@gmail.com>
2018-08-02 22:48:26 +0300

    Addtwig support.

--- html/index.php
+++ html/index.php
@@ -1,3 +1,15 @@
 <?php
 
 require_once 'autoload.php';
+
+// Use the Twig template engine in order to render a page.
+$loader = new Twig_Loader_Filesystem(getcwd() . '/templates');
+$twig = new Twig_Environment($loader, [
+  'cache' => 'twig_compilation_cache',
+  // For testing purposes only.
+  'auto_reload' => true,
+  'debug' => true,
+]);
+
+$twig->addExtension(new Twig_Extension_Debug());
+echo $twig->render('index.html');
